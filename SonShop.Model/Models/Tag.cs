4a59b1ea224e98d0f6769f2d3c77144090a6fe22
DESKTOP-ND4PQHK\duyng <duynguyenhd209@gmail.com>
2021-01-06 23:41:36 +0700

    add domain entity for project

---
+++ SonShop.Model/Models/Tag.cs
@@ -0,0 +1,27 @@
+﻿using SonShop.Model.Abstract;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace SonShop.Model.Models
+{
+    [Table("Tags")]
+    public class Tag
+    {
+        [Key]
+        [MaxLength(50)]
+        public string ID { set; get; }
+        [Required]
+        [MaxLength(50)]
+        public string Name { set; get; }
+        [Required]
+        [MaxLength(50)]
+        public string Type { set; get; }
+
+    }
+}
