cbe6ab68bc2cbdd1b2baecac72df54fbbc5ad665
DESKTOP-ND4PQHK\duyng <duynguyenhd209@gmail.com>
2021-01-10 22:37:59 +0700

    commit more service

---
+++ SonShop.Service/PostCategoryService.cs
@@ -0,0 +1,64 @@
+﻿using SonShop.Data.InfaStructure;
+using SonShop.Data.Repositories;
+using SonShop.Model.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SonShop.Service
+{
+    public interface IPostCategoryService
+    {
+        void Add(PostCategory postCategory);
+        void Update(PostCategory postCategory);
+        void Delete(int id);
+        IEnumerable<PostCategory> GetAll();
+        IEnumerable<PostCategory> GetAllByParentId(int parentId);
+        PostCategory GetByID(int id);
+
+
+
+    }
+    public class PostCategoryService : IPostCategoryService
+    {
+        IPostCategoryRepository _postCategoryRepository;
+        private IUnitOfWork _unitOfWork;
+
+        public PostCategoryService(IPostCategoryRepository postCategoryRepository, IUnitOfWork unitOfWork)
+        {
+            this._postCategoryRepository = postCategoryRepository;
+            this._unitOfWork = unitOfWork;
+        }
+        public void Add(PostCategory postCategory)
+        {
+            _postCategoryRepository.Add(postCategory);
+        }
+
+        public void Delete(int id)
+        {
+            _postCategoryRepository.Delete(id);
+        }
+
+        public IEnumerable<PostCategory> GetAll()
+        {
+            return _postCategoryRepository.GetAll();
+        }
+
+        public IEnumerable<PostCategory> GetAllByParentId(int parentId)
+        {
+            return _postCategoryRepository.GetMulti(x=> x.Status && x.ParentID == parentId);
+        }
+
+        public PostCategory GetByID(int id)
+        {
+            return _postCategoryRepository.GetSingleByID(id);
+        }
+
+        public void Update(PostCategory postCategory)
+        {
+             _postCategoryRepository.Update(postCategory);
+        }
+    }
+}
