c2f9e898a95af0522ed40ea770349f6d73537f02
DESKTOP-ND4PQHK\duyng <duynguyenhd209@gmail.com>
2021-01-13 22:31:07 +0700

    add web API

---
+++ SonShop.Data/Repositories/ErrorRepository.cs
@@ -0,0 +1,16 @@
+﻿using SonShop.Data.InfaStructure;
+using SonShop.Model.Models;
+
+namespace SonShop.Data.Repositories
+{
+    public interface IErrorRepository : IRepository<Error>
+    {
+    }
+
+    public class ErrorRepository : RepositoryBase<Error>, IErrorRepository
+    {
+        public ErrorRepository(IDbFactory dbFactory) : base(dbFactory)
+        {
+        }
+    }
+}
